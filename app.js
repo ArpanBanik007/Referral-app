290bcc0188184f6f2bd335389d4be89222c99399
Arpan Banik <arpanbanik976@gmail.com>
2025-08-04 11:55:12 +0530

    Fixed route from * to /* for Render deployment

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 
 app.use(express.static(path.join(__dirname, "Frontend", "Frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "Frontend", "Frontend", "dist", "index.html"));
 });
 
